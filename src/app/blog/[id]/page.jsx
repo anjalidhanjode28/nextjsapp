5a2afe87419291bd3658fcdfdec24e60f49b6ca9
anjalidhanjode28 <anjalidhanjode28@gmail.com>
2025-04-22 18:37:15 +0530

    Dark mode started

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import styles from "./page.module.css"
 
 const BlogPost = () => {
   return (
-    <div>
-    BlogPost
+    <div className={styles.container}>
+     <div className={styles.top}>
+      <div className={styles.info}>
+        <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
+        <p className={styles.desc}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur explicabo nihil expedita est maiores. Incidunt temporibus architecto nobis velit iure.</p>
+        <div className={styles.author}></div>
+      </div>
+     </div>
     </div>
   );
 }
---
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./DarkModeToggle.module.css"
+
+const DarkModeToggle = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default DarkModeToggle;
